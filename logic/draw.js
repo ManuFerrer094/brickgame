d30e1662053517f77fee9dd5d3acc6ccfb527d4b
ManuFerrer094 <manuferrer094@gmail.com>
2024-02-05 17:07:10 +0100

    Ball position

--- logic/draw.js
+++ logic/draw.js
@@ -42,6 +42,8 @@ window.onload = function() {
     drawButton("¿Quieres jugar?");
 };
 
+var ballStuckToPaddle = true;
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks(ctx, brickWidth, brickHeight, brickPadding, brickOffsetTop, brickOffsetLeft, bricks, brickRowCount, brickColumnCount);
@@ -54,6 +56,32 @@ function draw() {
     updatePowerUps(powerUps, powerUpSpeed, powerUpEffectDuration);
     paddlePowerUpCollision();
 
+    if (ballStuckToPaddle) {
+        // Dibujar la pelota en la posición inicial enganchada al paddle
+        x = paddleX + paddleWidth / 2;
+        y = canvas.height - paddleHeight - ballRadius;
+    }
+
+    // Escuchar eventos de teclado para liberar la pelota
+    document.addEventListener("keydown", function(event) {
+        if ((event.keyCode === 32 || event.key === " ") && ballStuckToPaddle) { // Tecla "Espacio"
+            ballStuckToPaddle = false; // Liberar la pelota
+            // Asignar velocidades a las direcciones x e y para que la pelota se mueva hacia arriba y hacia la izquierda o la derecha
+            dx = Math.random() < 0.5 ? -2 : 2; // Dirección horizontal aleatoria
+            dy = -2; // Dirección vertical hacia arriba
+        }
+    });
+
+    // Escuchar eventos de ratón para liberar la pelota al hacer clic izquierdo
+    canvas.addEventListener("click", function() {
+        if (ballStuckToPaddle) {
+            ballStuckToPaddle = false; // Liberar la pelota
+            // Asignar velocidades a las direcciones x e y para que la pelota se mueva hacia arriba y hacia la izquierda o la derecha
+            dx = Math.random() < 0.5 ? -2 : 2; // Dirección horizontal aleatoria
+            dy = -2; // Dirección vertical hacia arriba
+        }
+    });
+    
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
